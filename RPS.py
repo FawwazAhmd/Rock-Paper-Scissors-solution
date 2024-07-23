4bb960d7810dd72513991098034b29a6659f0ad9
Fawwaz Ahmed <fawwaz4000999@gmail.com>
2024-07-23 17:47:52 +0000

    Initial commit with Rock, Paper, Scissors solution

--- RPS.py
+++ RPS.py
@@ -1,10 +1,22 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
+#Predictive Strategy:
+
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play:
+        opponent_history.append(prev_play)
+    
+    if len(opponent_history) > 1:
+        last_move = opponent_history[-1]
+        second_last_move = opponent_history[-2]
+        
+        if last_move == 'R' and second_last_move == 'R':
+            return 'P'
+        elif last_move == 'P' and second_last_move == 'P':
+            return 'S'
+        elif last_move == 'S' and second_last_move == 'S':
+            return 'R'
+        else:
+            return 'R'  # Default move if no pattern detected
+    return 'R'  # Default move if no history
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
-
-    return guess
